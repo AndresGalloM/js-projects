e9c3d5abb899d3e9b77ea99d37946abe2f5a20d6
Andres Gallo <felipegallom1011@gmail.com>
2025-06-25 20:51:44 -0500

    DB model

--- drive-tutorial/src/server/db/schema.ts
+++ drive-tutorial/src/server/db/schema.ts
@@ -1,6 +1,41 @@
-import { int, singlestoreTable, varchar } from "drizzle-orm/singlestore-core";
-export const usersTable = singlestoreTable("users_table", {
-  id: int().primaryKey(),
-  name: varchar({ length: 255 }).notNull(),
-  age: int().notNull(),
-});
+import {
+  index,
+  int,
+  singlestoreTableCreator,
+  text,
+  varchar,
+  timestamp,
+  bigint,
+} from "drizzle-orm/singlestore-core";
+
+export const createTable = singlestoreTableCreator(
+  (name) => `drive_tutorial_${name}`,
+);
+
+export const filesTable = createTable(
+  "files",
+  {
+    id: bigint({ mode: "number" }).primaryKey().autoincrement(),
+    name: varchar({ length: 255 }).notNull(),
+    size: int().notNull(),
+    url: text().notNull(),
+    parent: bigint({ mode: "number" }).notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const foldersTable = createTable(
+  "folders",
+  {
+    id: bigint({ mode: "number" }).primaryKey().autoincrement(),
+    name: varchar({ length: 255 }).notNull(),
+    parent: bigint({ mode: "number" }),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
