dc727a9c10ef055e6f440a2d4bcb5227aa20ff28
Andres Gallo <felipegallom1011@gmail.com>
2025-07-01 18:42:29 -0500

    Fix right click menu

--- drive-tutorial/src/components/RightClickMenu.tsx
+++ drive-tutorial/src/components/RightClickMenu.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 import { Menu, MenuType } from "./Menu";
 import { ContextMenu, ContextMenuTrigger } from "./ui/context-menu";
 
@@ -9,8 +9,17 @@ export function RightClickMenu({
   children: JSX.Element;
   folderId: number;
 }) {
+  const [key, setKey] = useState(0);
+
   return (
-    <ContextMenu>
+    <ContextMenu
+      key={key}
+      onOpenChange={(open) => {
+        if (!open) {
+          setKey((prev) => prev + 1);
+        }
+      }}
+    >
       <ContextMenuTrigger asChild>{children}</ContextMenuTrigger>
       <Menu type={MenuType.context} folderId={folderId} />
     </ContextMenu>
